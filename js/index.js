b8eed350afb3b7d276b3a42175e94382abfeb62c
jbflores921 <77946185+jbflores921@users.noreply.github.com>
2021-07-17 10:37:54 -0500

    added html, css , and js for dark mode toggle

--- js/index.js
+++ js/index.js
@@ -2,14 +2,20 @@ console.log("Your index.js file is loaded correctly!");
 
 // preloader//
 
-jQuery(window).on('load', function () {
+$(window).on('load', function () {
     (function ($) {
     preloaderFadeOutInit();
-    })(jQuery);
+    });
     });
 
 //scroll to top//
  window.scrollTo({top: 0, behavior: "smooth"});
+
+ //dark mode//
+ function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
      
 
 
